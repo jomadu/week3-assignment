4396c3f643a58c92d9e272525b5087635a6cfa8b
Max Dunn <maxdunn123@gmail.com>
2021-04-24 15:52:25 -0700

    looking good. liked list working

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -1,11 +1,53 @@
 import "./App.css";
 import AirbnbCard from "./components/AirbnbCard/AirbnbCard";
+import Airbnbs from "./_data/airbnbs.json";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Layout = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+`;
+
+const CardLayout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
 
 function App() {
+  const [likedIndicies, setLikedIndicies] = useState([]);
+  const indicies = [...Array(Airbnbs.length).keys()];
+
+  const onLikeClickedHandler = (idx) => {
+    return () => {
+      if (likedIndicies.includes(idx)) {
+        setLikedIndicies([
+          ...likedIndicies.filter((likedIdx) => idx != likedIdx),
+        ]);
+      } else {
+        setLikedIndicies([...likedIndicies, idx]);
+      }
+    };
+  };
+
+  const cards = Airbnbs.map((_, idx) => (
+    <AirbnbCard
+      idx={idx}
+      key={idx}
+      onLikeClicked={onLikeClickedHandler(idx)}
+      liked={likedIndicies.includes(idx)}
+    />
+  ));
+
+  const liked = Airbnbs.filter((_, idx) =>
+    likedIndicies.includes(idx)
+  ).map((airbnb) => <li>{airbnb.title}</li>);
+
   return (
-    <div className="App">
-      <AirbnbCard idx={0} />
-    </div>
+    <Layout>
+      <CardLayout>{cards}</CardLayout>
+      <ul>{liked}</ul>
+    </Layout>
   );
 }
 
--- ecommerce-ui/src/components/AirbnbCard/AirbnbCard.js
+++ ecommerce-ui/src/components/AirbnbCard/AirbnbCard.js
@@ -1,41 +1,120 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./AirbnbCard.css";
 import Airbnbs from "../../_data/airbnbs.json";
+import styled from "styled-components";
 
-function AirbnbCard({ idx }) {
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { BiStar } from "react-icons/bi";
+
+const Card = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  background-color: salmon;
+  height: 500px;
+  margin: 10px 0px 10px 0px;
+`;
+const CardMedia = styled.div`
+  width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: 50% 50%;
+    border-radius: 10px;
+  }
+`;
+const CardContent = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-between;
+  padding: 15px;
+  flex-grow: 2;
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  height: 150px;
+`;
+
+const CardHeaderText = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-between;
+`;
+
+const CardDescription = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+const CardFooter = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const CardFooterPayment = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+const StyledSvg = styled.div`
+  svg {
+    fill: ${(props) => props.color};
+  }
+`;
+
+function AirbnbCard({ idx, onLikeClicked, liked }) {
   const airbnb = Airbnbs[idx];
   const { host, houseType, image, location, payment, rating, title } = airbnb;
-  console.log(airbnb);
   return (
-    <div className="AirbnbCard">
-      <div className="AirbnbCardHeader">
-        <p>{title}</p>
-        <p>{houseType}</p>
-      </div>
-      <div className="AirbnbCardMedia">
-        <img src={image} />
-      </div>
-      <div className="AirbnbCardRatingsReviews">
-        <p>{rating.stars}</p>
-        <p>{rating.reviews}</p>
-      </div>
-      <div className="AirbnbCardLocationPayment">
-        <p>{location.city}</p>
-        <p>{location.country}</p>
-        <p>{payment.cost}</p>
-        <p>{payment.description}</p>
-      </div>
-      <div className="AirbnbCardHost">
-        <p>{host.name}</p>
-        <p>{host.isSuperhost ? "SuperHost" : "Not SuperHost"}</p>
-      </div>
-    </div>
+    <Card>
+      <CardMedia>
+        <img src={image} alt={image} />
+      </CardMedia>
+      <CardContent>
+        <CardHeader>
+          <CardHeaderText>
+            <div>
+              {houseType} in {location.city},{location.country}
+            </div>
+            <div>{title}</div>
+          </CardHeaderText>
+          <StyledSvg onClick={onLikeClicked} color={"black"}>
+            {liked ? <AiFillHeart size={40} /> : <AiOutlineHeart size={40} />}
+          </StyledSvg>
+        </CardHeader>
+        <CardDescription>
+          <div>
+            {host.name}
+            {host.isSuperhost ? " - SuperHost" : ""}
+          </div>
+        </CardDescription>
+        <CardFooter>
+          <div>
+            <BiStar /> {rating.stars} ({rating.reviews} reviews)
+          </div>
+          <CardFooterPayment>
+            <div>${payment.cost} / night</div>
+            {payment.description === "" ? null : (
+              <div>{payment.description}</div>
+            )}
+          </CardFooterPayment>
+        </CardFooter>
+      </CardContent>
+    </Card>
   );
 }
 
 AirbnbCard.propTypes = {
   idx: PropTypes.number,
+  onLikeClicked: PropTypes.func,
+  liked: PropTypes.bool,
 };
 
 export default AirbnbCard;
